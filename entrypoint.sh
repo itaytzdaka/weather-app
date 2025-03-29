cc725aaf8a08c8bbecdf7ff95dc273cea956df2a
itaytzdaka <itaytzdaka1@gmail.com>
2025-03-30 01:36:08 +0300

    Remove Docker files from Git, keep locally

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace env vars in nginx.template.conf and save as nginx.conf
-envsubst '${SERVER} ${PORT}' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf
-
-# Start nginx
-exec nginx -g 'daemon off;'
