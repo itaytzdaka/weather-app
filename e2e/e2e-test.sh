d56a34a42903e82b5cb54707a270a6de1fcac47d
itaytzdaka <itaytzdaka1@gmail.com>
2025-06-20 13:47:16 +0300

    change e2e test

--- e2e/e2e-test.sh
+++ e2e/e2e-test.sh
@@ -1,3 +1,20 @@
+# #!/bin/bash
+
+# set -e
+
+# URL="http://nginx/api/forecast/tel%20aviv"
+
+# RESPONSE=$(curl -s "$URL")
+
+# if echo "$RESPONSE" | jq . > /dev/null 2>&1; then
+#     echo "✅ Got valid JSON"
+#     exit 0
+# else
+#     echo "❌ Invalid JSON response:"
+#     exit 1
+# fi
+
+
 #!/bin/bash
 set -e
 
@@ -6,11 +23,11 @@ URL="http://${HOSTNAME}/api/forecast/tel%20aviv"
 
 RESPONSE=$(curl -s "$URL")
 
-if echo "$RESPONSE" | jq . > /dev/null 2>&1; then
-    echo "✅ Got valid JSON"
+
+if echo "$RESPONSE" | grep -q '"name":"Tel Aviv-Yafo"'; then
+    echo "✅ Found expected location in JSON response"
     exit 0
 else
-    echo "❌ Invalid JSON response:"
-    echo "$RESPONSE"
+    echo "❌ Expected value not found in response:"
     exit 1
 fi
